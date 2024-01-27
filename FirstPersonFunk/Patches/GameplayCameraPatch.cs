78f676f5a93ffcf29e5e6354f1a068c6e8b7d660
NotNite <hi@notnite.com>
2024-01-27 07:36:22 -0500

    lmao

---
+++ FirstPersonFunk/Patches/GameplayCameraPatch.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using Reptile;
+using UnityEngine;
+
+namespace FirstPersonFunk.Patches;
+
+[HarmonyPatch(typeof(GameplayCamera))]
+public class GameplayCameraPatch {
+    private static Vector2 Offset = new(0, 0);
+
+    [HarmonyPrefix]
+    [HarmonyPatch("UpdateCamera")]
+    public static bool UpdateCamera(GameplayCamera __instance) {
+        __instance.UpdateCameraInput();
+
+        var tf = __instance.transform;
+        var head = __instance.player.headTf;
+        tf.position = head.position;
+
+        var input = __instance.orbitInput *
+                    (__instance.mouseForOrbit ? __instance.mouseFactor : __instance.joystickFactor);
+
+        if (Plugin.RotateWithHead.Value) {
+            Offset.x += input.x;
+            Offset.y += input.y;
+            Offset.y = Clamp(Offset.y);
+
+            var offsetQuat = Quaternion.Euler(new Vector3(Offset.y, Offset.x, 0));
+            // Pretend the head bone is rotated
+            var fakeHead = head.rotation * Quaternion.Euler(new Vector3(0, 0, 90));
+            tf.rotation = fakeHead * offsetQuat;
+        } else {
+            var euler = tf.rotation.eulerAngles;
+            euler.x += input.y;
+            euler.y += input.x;
+            euler.z = 0;
+            euler.x = Clamp(euler.x);
+
+            tf.rotation = Quaternion.Euler(euler);
+        }
+
+        return false;
+    }
+
+    // Clamp code I stole from BombRushCamera because it's 7 AM and I forgot how this works
+    private static float Clamp(float wtf) {
+        wtf = wtf > 180 ? wtf - 360 : wtf;
+        wtf = Mathf.Clamp(wtf, -89, 89);
+        return (wtf + 360) % 360;
+    }
+}
---
+++ FirstPersonFunk/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Reptile;
+
+namespace FirstPersonFunk;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Bomb Rush Cyberfunk.exe")]
+public class Plugin : BaseUnityPlugin {
+    public static ManualLogSource Log = null!;
+    public static Harmony Harmony = null!;
+    public static ConfigEntry<bool> ShowPlayer = null!;
+    public static ConfigEntry<bool> RotateWithHead = null!;
+
+    private void Awake() {
+        Log = this.Logger;
+        Harmony = new Harmony("FirstPersonFunk.Harmony");
+        Harmony.PatchAll();
+
+        ShowPlayer = this.Config.Bind(
+            "General",
+            "ShowPlayer",
+            true,
+            "Whether to show the player character in first person mode."
+        );
+        RotateWithHead = this.Config.Bind(
+            "General",
+            "RotateWithHead",
+            false,
+            "Whether to rotate the camera with the player's head, or to only move it when you move it."
+        );
+    }
+
+    private void Update() {
+        if (ShowPlayer.Value) return;
+        if (WorldHandler.instance == null) return;
+        var player = WorldHandler.instance.GetCurrentPlayer();
+        if (player == null) return;
+        player.characterVisual.mainRenderer.enabled = player.cam == null;
+    }
+}
